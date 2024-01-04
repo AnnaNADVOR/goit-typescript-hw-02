b04079893b9f2298bbfc7ae37b3439ea14ec4bac
Anna <anjanadvorna@gmail.com>
2024-01-04 14:31:00 +0200

    update

--- dist/baseTypes/8.js
+++ dist/baseTypes/8.js
@@ -1,6 +1,3 @@
 const myGender = "male";
-function sayHello(name) { console.log(`Hello, ${name}!`); }
-let result = sayHello("John");
-console.log(result);
 export {};
 //# sourceMappingURL=8.js.map
--- dist/generic/8.js
+++ dist/generic/8.js
@@ -2,5 +2,7 @@
   У вас є тип Form, який містить інформацію про форму, включаючи поле errors.
   Ви хочете створити новий тип Params, який включає всі поля з Form, крім errors.
 */
-export {};
+// // Реалізуйте Params так, щоб унеможливити поле 'errors' з типу Form
+// type Params = Form;
+// export {};
 //# sourceMappingURL=8.js.map
