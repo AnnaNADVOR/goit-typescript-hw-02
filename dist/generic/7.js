c94f28e49c0ae033ba53e8ee2e93217ee56901f7
Anna <anjanadvorna@gmail.com>
2024-01-04 16:46:54 +0200

    update

--- dist/generic/7.js
+++ dist/generic/7.js
@@ -10,8 +10,8 @@ export var UserRole;
 })(UserRole || (UserRole = {}));
 // Замініть наступний код на версію за допомогою Record
 const RoleDescription = {
-    admin: 'Admin User',
-    editor: 'Editor User',
-    guest: 'Guest User',
+    [UserRole.admin]: 'Admin User',
+    [UserRole.editor]: 'Editor User',
+    [UserRole.guest]: 'Guest User',
 };
 //# sourceMappingURL=7.js.map
--- dist/generic/8.js
+++ dist/generic/8.js
@@ -2,7 +2,5 @@
   У вас є тип Form, який містить інформацію про форму, включаючи поле errors.
   Ви хочете створити новий тип Params, який включає всі поля з Form, крім errors.
 */
-// // Реалізуйте Params так, щоб унеможливити поле 'errors' з типу Form
-// type Params = Form;
-// export {};
+export {};
 //# sourceMappingURL=8.js.map
